b343672615304ad344bcaa50ec2bacc32d226cfc
gabamia <gabaguevara@gmail.com>
2012-11-24 18:10:58 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventosSebastian::Application.config.secret_token = '9496ee4fd77d92813933898531537e8aa8e5f3eb0502cc79ef85f70ed02c2e8eaa8819eaaf1b3e44e6bea678e70166ae9288edcece0e0e669ee0b5b0e739ad6f'
